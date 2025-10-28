7502af3bf63f772c66dc233509db931e3d40b5a3
ebranquinho <branquinhomanu@gmail.com>
2025-10-28 16:02:56 -0300

    fix kafka configuration

--- src/Zeus.Orders.Shipping.Worker/Program.cs
+++ src/Zeus.Orders.Shipping.Worker/Program.cs
@@ -1,7 +1,6 @@
-using Zeus.Orders.Shipping.Worker;
-
 var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddHostedService<Worker>();
+builder.Services.Configure<KafkaSettings>(builder.Configuration.GetSection("Kafka"));
 
 var host = builder.Build();
 host.Run();
--- src/Zeus.Orders.Shipping.Worker/Usings.cs
+++ src/Zeus.Orders.Shipping.Worker/Usings.cs
@@ -2,4 +2,5 @@ global using System.Text.Json;
 global using Confluent.Kafka;
 global using Microsoft.Extensions.Options;
 global using Zeus.Orders.Shared.Events;
+global using Zeus.Orders.Shipping.Worker;
 global using Zeus.Orders.Shipping.Worker.Configuration;
